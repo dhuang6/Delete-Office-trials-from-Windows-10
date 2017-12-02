bf8cc11b292452a2be15232cc4be13787e291a3b
Daniel Huang <dhuang684@gmail.com>
2017-12-01 19:50:03 -0600

    update to remove
    Click-to-Run Extensibility Component

--- deleteregistrykeys.bat
+++ deleteregistrykeys.bat
@@ -9,3 +9,7 @@ reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninsta
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\O365HomePremRetail - fr-fr" /f
 
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Office" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Features\00006109C80000000000000000F01FEC" /f
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Products\00006109C80000000000000000F01FEC" /f
